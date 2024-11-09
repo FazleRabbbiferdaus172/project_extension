5697bd70da9c8f8e2be5b992be905e126b84976f
Fazle Rabbi Ferdaus <rabbi.ferdaus@brainstation-23.com>
2024-11-10 02:52:42 +0600

    [ADD] final commit with manifest, init and access updates
    final commit with manifest, init and access updates

--- project_extension/__manifest__.py
+++ project_extension/__manifest__.py
@@ -12,6 +12,18 @@
                 - Create Teams.
                 - Assign teams to projects.
                 - Dashboard.
+                
+        Feature description:
+            1. Team can be created by project managers from the menu "root > Project > Teams. Members can be added to the team from form view.
+            2. Projects are only visible to the "projects users" that are part of the assigned team of that project. 
+            3. Project manages are considered admin of project module and has access to all project regardless. 
+            2. Dashboard is accessible "Root > Dashboard > Project"
+            3. Dashboard shows 2 tables 
+                - Project task count table: open tasks count of each project and assigned team
+                - Assignee task table: Assigned task of each user
+            4. Dashboard has 2 filters that takes effect on the 2nd table
+                - Period filter can be used to view task for each period of last 7, 30, 90, 185, 365 days and last 3 years.
+                - Assignee filter can be used to view task for each assigned project and assigned team
     """,
 
     'author': "Fazle Rabbi Ferdaus",
@@ -19,12 +31,15 @@
     'category': 'Uncategorized',
     'version': '0.0.1',
 
-    'depends': ['project'],
+    'depends': ['project', 'spreadsheet_dashboard'],
     'installable': True,
     'application': True,
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        "data/dashboard.xml",
+        'security/ir.model.access.csv',
+        'views/views_project.xml',
+        'report/task_assignee_project_report_view.xml'
     ],
     # only loaded in demonstration mode
     # 'demo': [
---
+++ project_extension/report/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import task_assignee_project_report
